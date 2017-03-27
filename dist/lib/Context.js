e6da42336913d89ed6a77b3dfd4271ae428b8fb6
Nitin Bansal <nitinbansal1989@yahoo.co.in>
2017-03-27 13:42:13 +0530

    added async in execute func

--- dist/lib/Context.js
+++ dist/lib/Context.js
@@ -55,8 +55,8 @@ class Context {
     setEntityPath(entityPath) {
         this.entityPath = entityPath;
     }
-    execute(query, args) {
-        return this.handler.run(query, args, this.connection);
+    async execute(query, args) {
+        return await this.handler.run(query, args, this.connection);
     }
     getCriteria() {
         return new Query.SqlExpression();
