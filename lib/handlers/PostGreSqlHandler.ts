f5f1e5711a1fbaf2215e3659045364fac7369f40
Nitin Bansal <nitin.bansal@moveinsync.com>
2017-12-12 14:43:53 +0530

    postgresql handler fix

--- lib/handlers/PostGreSqlHandler.ts
+++ lib/handlers/PostGreSqlHandler.ts
@@ -46,43 +46,43 @@ class PostGreHandler extends Handler.default {
 
 	async	close(conn) { conn.release() }
 
-	getTableInfo(tableName: string): Array<Handler.ColumnInfo> {
-		let describeTableQuery = fs.readFileSync('../../assets/postgresql_describe_query.sql', 'utf-8');
+	getTableInfo(tableName: string) {
+		let describeTableQuery = fs.readFileSync(__dirname + '/../../assets/postgresql_describe_query.sql', 'utf-8');
 		let descQuery = describeTableQuery.replace('?', tableName);
 		let tableInfo = util.deAsync(this.run(descQuery));
 		let result: Array<Handler.ColumnInfo> = new Array<Handler.ColumnInfo>();
 
 		tableInfo.rows.forEach((row) => {
-			let aObj: Handler.ColumnInfo = new Handler.ColumnInfo();
-			aObj.field = row["field"];
+			let obj: Handler.ColumnInfo = new Handler.ColumnInfo();
+			obj.field = row["field"];
 			let columnType: string = (<string>row["data_type"]).toLowerCase();
 
-			if (columnType.includes("int") ||
+			if (columnType.includes("boolean")) {
+				obj.type = "boolean";
+			} else if (columnType.includes("int") ||
 				columnType.includes("float") ||
 				columnType.includes("double") ||
 				columnType.includes("decimal") ||
 				columnType.includes("real") ||
 				columnType.includes("numeric")) {
-				aObj.type = "number";
-			}
-			else if (columnType.includes("varchar") ||
+				obj.type = "number";
+			} else if (columnType.includes("varchar") ||
 				columnType.includes("text") ||
-				columnType.includes("character varying")) {
-				aObj.type = "string";
+				columnType.includes("character varying") ||
+				columnType.includes("uuid")) {
+				obj.type = "string";
+			} else if (columnType.includes("timestamp") || columnType.includes("date")) {
+				obj.type = "date";
 			}
-			else if (columnType.includes("timestamp") || columnType.includes("date")) {
-				aObj.type = "date";
-			}
-
-			aObj.nullable = !row["notnull"];
-			aObj.primaryKey = row["primarykey"];
-			aObj.default = row["default"];
-			result.push(aObj);
+			obj.nullable = !row["notnull"];
+			obj.primaryKey = row["primarykey"];
+			obj.default = row["default"];
+			result.push(obj);
 		});
 		return result;
 	}
 
-	async run(query: string | Query.ISqlNode, args?: Array<any>, connection?: Connection): Promise<Handler.ResultSet> {
+	async run(query: string | Query.ISqlNode, args?: Array<any>, connection?: Connection) {
 		let q: string = null;
 		if (typeof query === "string") {
 			q = query;
@@ -92,28 +92,38 @@ class PostGreHandler extends Handler.default {
 		}
 
 		let result: Handler.ResultSet = new Handler.ResultSet();
+		let con = null;
 		if (connection && connection instanceof Connection && connection.Handler.handlerName == this.handlerName && connection.conn) {
-			var r = await connection.conn.query(q, args);
+			con = connection.conn;
 		} else {
-			let r = await this.connectionPool.query(q, args);
+			con = this.connectionPool;
 		}
+		let p = new Promise((resolve, reject) => {
+			con.query(q, args, (err, response) => {
+				if (err) {
+					reject(err)
+				} else {
+					resolve(response)
+				}
+			});
+		});
+		let r: any = await p;
 		if (r.rowCount) result.rowCount = r.rowCount;
 		if (Array.isArray(r.rows)) result.rows = r.rows.slice();
 		if (Array.isArray(r.rows) && r.rows.length > 0) result.id = r.rows[0].id;
 		return result;
 	}
 
-	convertPlaceHolder(query: string): string {
-		let result: string = '';
-		let index = 0;
-		query.replace('?', (a) => {
-			index++;
-			return '$' + index;
-		});
-		return result;
+	convertPlaceHolder(query: string) {
+		for (let i = 1; query.includes('?'); i++) {
+			query = query.replace('?', '$' + i);
+		}
+		return query;
 	}
 
-	insertQuery(collection: string, columns: string, values: string): string { return super.insertQuery(collection, columns, values) + ' returning id'; }
+	insertQuery(collection: string, columns: string, values: string) {
+		return super.insertQuery(collection, columns, values) + ' returning id';
+	}
 }
 
 export default PostGreHandler;
