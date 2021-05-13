e1d490ed6d8ad9ba56618762e95a7c9a6a466b04
Nitin Bansal <1055173+nitinbansal@users.noreply.github.com>
2021-05-13 19:22:24 +0530

    Added util functions

--- dist/Util.js
+++ dist/Util.js
@@ -1,9 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCriteria = void 0;
+const types = require("./types");
 const sql = require("./sql");
-function getCriteria() {
-    return new sql.Expression();
+const moment = require("moment");
+function convert(res, ignoreKeys, ...srcs) {
+    ignoreKeys || (ignoreKeys = []);
+    srcs.forEach(src => {
+        Reflect.ownKeys(src).filter((key) => {
+            return !ignoreKeys.includes(key)
+                && src[key] != null
+                && res[key] instanceof sql.Field
+                && res[key].get() != src[key];
+        }).forEach((key) => {
+            if (res[key] instanceof types.Date) {
+                let d = moment(src[key]);
+                res[key].set(d.toDate());
+            }
+            else {
+                res[key].set(src[key]);
+            }
+        });
+    });
+    return res;
 }
-exports.getCriteria = getCriteria;
-//# sourceMappingURL=Util.js.map
+function reverse(res, ignoreKeys, ...srcs) {
+    ignoreKeys || (ignoreKeys = []);
+    srcs.forEach(src => {
+        Reflect.ownKeys(src).filter((key) => {
+            return !ignoreKeys.includes(key)
+                && src[key] instanceof sql.Field
+                && res[key] != src[key].get();
+        }).forEach((key) => {
+            res[key] = src[key].get();
+        });
+    });
+    return res;
+}
+exports.default = {
+    convert,
+    reverse
+};
+//# sourceMappingURL=util.js.map
