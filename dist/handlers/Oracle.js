619c940eddac91006deb330ef8efcac0d95ed4c6
Nitin Bansal <nitin.bansal@moveinsync.com>
2024-01-16 13:19:05 +0530

    multi query join fix

--- dist/handlers/Oracle.js
+++ dist/handlers/Oracle.js
@@ -33,7 +33,7 @@ export default class Oracle extends Handler {
                 tempQueries.push(a.eval(this));
                 dataArgs.push(...a.args);
             });
-            query = tempQueries.join('; ');
+            query = tempQueries.join('; ').concat(';');
         }
         else if (queryStmt instanceof sql.Statement) {
             query = queryStmt.eval(this);
